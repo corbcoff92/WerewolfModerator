01a64735d23d002fdf18509ade96fb03c4b627d0
Corbyn Coffin <corbcoff92@gmail.com>
2022-03-03 14:44:04 -0700

    Shared docstrings

--- shared.py
+++ shared.py
@@ -1,13 +1,13 @@
 from random import randint
 from enum import Enum
 import tkinter as tk
-from tkinter.messagebox import showerror
 
 
 HOST, PORT = 'localhost', 55555
 
 
 class Roles(Enum):
+    """ Roles for a game of werewolf. """
     VILLAGER = 'VILLAGER'
     WEREWOLF = 'WEREWOLF'
     DOCTOR = 'DOCTOR'
@@ -18,7 +18,22 @@ class Roles(Enum):
 
 
 class WerewolfModeratorClientDisplay(tk.Frame):
-    def __init__(self, frame):
+    """ 
+    Tkinter frame containing a listbox for displaying clients.
+    
+    Extends:
+            tkinter.Frame
+    """
+    def __init__(self, frame: tk.Frame) -> None:
+        """
+        Creates an instance of a WerewolfModeratorClientDisplay.
+
+        This frame contains a listbox for displaying clients.
+        
+        Arguments:
+            frame: tkinter.Frame
+                Frame to which this client display should be added.
+        """
         super().__init__(master=frame)
         self.client_names = []
 
@@ -31,40 +46,57 @@ class WerewolfModeratorClientDisplay(tk.Frame):
         self.clients_lb.config(yscrollcommand=self.scroll_bar.set)
         self.clients_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
     
-    def clear(self):
+    def clear(self) -> None:
+        """ Clears the client display list box. """
         self.clients_lb.config(state=tk.NORMAL)
         self.clients_lb.delete(0, tk.END)
 
-    def update(self, client_names, excluded_clients=[]):
+    def update(self, client_names: list[str], excluded_clients: list[str]=[]) -> None:
+        """
+        Updates the clients display listbox to reflect all of the clients in the given list of clients that
+        are not contained in the optional excluded clients list.
+
+        Aruments:
+            client_names: list[str]
+                Names of the clients that should be displayed and allowed for selection.
+        Optional:
+            excluded_clients: list[str], Default=[]
+                Names of the clients that should not be displayed and allowed for selection.
+        """
         self.clear()
         self.client_names = [name for name in client_names if name not in excluded_clients]
         self.clients_lb.insert(tk.END, *[name for name in self.client_names])
         self.clients_lb.config(state=tk.DISABLED)
 
 
-class WerewolfModeratorSelectableClientDisplay(WerewolfModeratorClientDisplay):
-    def __init__(self, frame, werewolf_moderator):
-        super().__init__(frame)
-        self.werewolf_moderator = werewolf_moderator
-
-    def update(self, client_names, excluded_clients=[]):
-        super().update(client_names=client_names, excluded_clients=excluded_clients)
-        self.clients_lb.config(state=tk.NORMAL)
-        
-    def select_client(self):
-        selection = self.clients_lb.curselection()
-        if selection:
-            idx = selection[0]
-            self.werewolf_moderator.selected_player = self.client_names[idx]
-        else:
-            showerror(title='No Selection', message='You must select an item...')
-
-
 class WerewolfModeratorClientRolesDisplay(WerewolfModeratorClientDisplay):
-    def __init__(self, frame):
+    """ 
+    Tkinter frame containing a listbox for displaying clients and thier respective werewolf roles.
+    
+    Extends:
+            tkinter.Frame
+    """
+    def __init__(self, frame: tk.Frame) -> None:
+        """
+        Creates an instance of a WerewolfModeratorClientRolesDisplay.
+
+        This frame contains a listbox for displaying clients and thier respective werewolf roles.
+        
+        Arguments:
+            frame: tkinter.Frame
+                Frame to which this client display should be added.
+        """
         super().__init__(frame)
         
-    def update(self, clients):
+    def update(self, clients: list[dict]) -> None:
+        """
+        Updates the clients display listbox to reflect all of the names and werewolf 
+        roles for the clients in the given list of client dictionaries.
+
+        Aruments:
+            clients: list[dict]
+                List of dictionaries containing client information, including their name and respective werewolf role.
+        """
         self.clear()
 
         werewolves = [client for client in clients if client['role'] == Roles.WEREWOLF]
@@ -85,9 +117,19 @@ class WerewolfModeratorClientRolesDisplay(WerewolfModeratorClientDisplay):
         self.clients_lb.config(state=tk.DISABLED)
 
 
-def shuffle_list(l):
-    num_items = len(l)
+def shuffle_list(list_to_shuffle: list) -> None:
+    """ 
+    Shuffles the given list in-place.
+
+    List is shuffled by swapping each item with a randomly selected item,
+    which is repreated five times.
+
+    Arguments:
+        list_to_shuffle: list
+            List that is to be shuffled in-place.
+    """
+    num_items = len(list_to_shuffle)
     for _ in range(5):
-        for item in l:
+        for item in list_to_shuffle:
             idx = randint(0, num_items - 1)
-            l.append(l.pop(idx))
+            list_to_shuffle.append(list_to_shuffle.pop(idx))
