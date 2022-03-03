40c0a31e2abc05f4d792047b72969ff7a84fc89a
Corbyn Coffin <corbcoff92@gmail.com>
2022-03-03 16:25:21 -0700

    Shared documentation

--- shared.py
+++ shared.py
@@ -1,8 +1,8 @@
-from random import randint
 from enum import Enum
+from random import randint
 import tkinter as tk
 
-
+# IP address & port number for server & clients
 HOST, PORT = 'localhost', 55555
 
 
@@ -37,6 +37,7 @@ class WerewolfModeratorClientDisplay(tk.Frame):
         super().__init__(master=frame)
         self.client_names = []
 
+        # Clients display
         self.clients_frame = tk.Frame(master=self)
         self.clients_lb = tk.Listbox(master=self.clients_frame, selectmode=tk.SINGLE, font=('Consolas', 14), height=5)
         self.clients_lb.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
@@ -48,6 +49,7 @@ class WerewolfModeratorClientDisplay(tk.Frame):
     
     def clear(self) -> None:
         """ Clears the client display list box. """
+        # Enable clients display for editing
         self.clients_lb.config(state=tk.NORMAL)
         self.clients_lb.delete(0, tk.END)
 
@@ -64,8 +66,13 @@ class WerewolfModeratorClientDisplay(tk.Frame):
                 Names of the clients that should not be displayed and allowed for selection.
         """
         self.clear()
+
+        # Determine unexcluded clients
         self.client_names = [name for name in client_names if name not in excluded_clients]
+        
         self.clients_lb.insert(tk.END, *[name for name in self.client_names])
+        
+        # Disable selectability of clients display
         self.clients_lb.config(state=tk.DISABLED)
 
 
@@ -99,21 +106,27 @@ class WerewolfModeratorClientRolesDisplay(WerewolfModeratorClientDisplay):
         """
         self.clear()
 
+        # Determine werewolves and villagers
         werewolves = [client for client in clients if client['role'] == Roles.WEREWOLF]
         villagers = [client for client in clients if client['role'] != Roles.WEREWOLF]
 
+        # Enable listbox for editing
         self.clients_lb.config(state=tk.NORMAL)
- 
+
+        # Calculate width of name column
         max_name_len = len(max(clients, key=lambda client: len(client['name']))['name']) + 2
         
+        # Display villagers
         self.clients_lb.insert(tk.END, f'{" VILLAGERS ":^25}')
         for client in villagers:
             self.clients_lb.insert(tk.END, f'{client["name"].ljust(max_name_len)}{client["role"].value.capitalize()}')
         
+        # Display werewolves
         self.clients_lb.insert(tk.END, f'{" WEREWOLVES ":^25}\n')
         for client in werewolves:
             self.clients_lb.insert(tk.END, f'{client["name"].ljust(max_name_len)}{client["role"].value.capitalize()}')
 
+        # Disable selectability of clients display
         self.clients_lb.config(state=tk.DISABLED)
 
 
